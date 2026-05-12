226e3c8b83d167f4dd589fd293d7ee2f62b88d45
Milad Rafiee <mld.rafiee@gmail.com>
2026-05-12 21:01:24 +0200

    initial commit

---
+++ data/database.js
@@ -0,0 +1,34 @@
+import { MongoClient } from 'mongodb';
+
+const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const dbUser = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DB_NAME;
+
+//const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb://${dbUser}:${dbPassword}@localhost:27017/${dbName}authSource=admin&directConnection=true&serverSelectionTimeoutMS=5000`;
+
+const client = new MongoClient(uri);
+
+
+
+
+
+try {
+  client = new MongoClient(uri);
+  console.log('Hang On! Connecting to your DB...');
+  await client.connect();
+  console.log('Connected to your DB Server');
+  const database = client.db(dbName);
+  await database.command({ ping: 1 });
+
+  console.log('Ping Successful!');
+
+} catch (error) {
+  console.log('Oops! Connection failed.');
+  process.exit(1);
+}
+
+
+
+export default database;
