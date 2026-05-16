ce0c5cda52f72d443e8a83f0a02af6a4d3e619e3
Milad Rafiee <mld.rafiee@gmail.com>
2026-05-16 20:09:29 +0200

    updated secrets

--- data/database.js
+++ data/database.js
@@ -5,15 +5,11 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-// const clusterAddress = "cluster0.39rg914.mongodb.net"
-// const dbUser = "db_action"
-// const dbPassword = "zRk05WNyV63R5Gha"
-// const dbName = "mongodb"
 
 //const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-const uri = `mongodb://${dbUser}:${dbPassword}@localhost:27017/${dbName}authSource=admin&directConnection=true&serverSelectionTimeoutMS=5000`;
+// const uri = `mongodb://${dbUser}:${dbPassword}@localhost:27017/${dbName}authSource=admin&directConnection=true&serverSelectionTimeoutMS=5000`;
 // const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://${encodeURIComponent(dbUser)}:${encodeURIComponent(dbPassword)}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${encodeURIComponent(dbUser)}:${encodeURIComponent(dbPassword)}@${clusterAddress}/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
 
